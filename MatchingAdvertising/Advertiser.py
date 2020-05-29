a79cfb1436da4f5aad462554ed414e5bd8c97d61
daniele96l <daniele96ligato@gmail.com>
2020-05-30 00:08:36 +0200

    basic auction process

--- MatchingAdvertising/Advertiser.py
+++ MatchingAdvertising/Advertiser.py
@@ -2,6 +2,7 @@ import numpy as np
 
 
 class Advertiser:
-    def __init__(self, bid, publisher):
+    def __init__(self, bid, publisher,budget):
         self.bid = bid
         self.publisher = publisher
+        self.budget = budget
--- MatchingAdvertising/VCG_auction.py
+++ MatchingAdvertising/VCG_auction.py
@@ -1,9 +1,34 @@
-
+import numpy as np
 
 class VCG_auction():
 
-    def auction(self, bids,advertisers,N_SLOTS):
-        winners = list[[advertisers for _,advertisers in sorted(zip(bids,advertisers))]: N_SLOTS] #take the higher paying n advertiser
-        value_to_pay = bids[N_SLOTS+1] #accordinly to vcg they will pay the "second highest price", in this case since we have n bid it's not the secondo but the n+1
-        return winners,value_to_pay
+    def set_bid(self,advertisers):
+        bids = []
+        for a in range(advertisers):
+            bids.append(advertisers.budget/np.random.uniform(20,50)) #each advertiser makes a small bid compared to his budget
+        return bids
 
+    def auction(self,bids,advertisers,N_SLOTS): #how the auction is hanled according to vcg
+        index_of_advertiser = []
+        for i in range(advertisers):
+            index_of_advertiser.append(i)
+        index_of_winners = list[[index_of_advertiser for _,index_of_advertiser in sorted(zip(-bids,index_of_advertiser))]: N_SLOTS] #take the higher paying n advertiser
+        bids = -np.sort(-bids) #descending order
+        value_to_pay = bids[N_SLOTS+1] #accordinly to vcg they will pay the "second highest price", in this case since we have n bid it's not the secondo but the n+1
+        return index_of_winners,value_to_pay
+
+    def choosing_the_slot(self,index_of_winners,q, N_SLOTS):
+        ## Advertiser 1 - [Slot1, Slot2, Slot3, Slot4]
+        ## Advertiser 2 - [Slot1, Slot2, Slot3, Slot4]
+        ## Advertiser 3 - [Slot1, Slot2, Slot3, Slot4]
+        ## Advertiser 4 - [Slot1, Slot2, Slot3, Slot4]
+        print(q, "not sorted")
+        q = -np.sort(-q)
+        print(q, "sorted")   #the first q of each row will have the higher probability of being clicked
+        #only the first winner will have the possibility to chose the first slot
+        #the secondo winner will choose the second and go on
+        for a in range(index_of_winners):  #the
+            for s in range(N_SLOTS):
+                allocated = q[a][index_of_winners]
+
+        return allocated
