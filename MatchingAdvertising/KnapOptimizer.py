526c883de6a7427fe9530153d7845f4a5a41dd74
daniele96l <daniele96ligato@gmail.com>
2020-06-27 21:23:11 +0200

    Knap

--- MatchingAdvertising/KnapOptimizer.py
+++ MatchingAdvertising/KnapOptimizer.py
@@ -1,51 +1,64 @@
 import numpy as np
 
+
 class KnapOptimizer():
-    def __init__(self,n_bids, n_budget, n_subcampaign):
+    def __init__(self, n_bids, n_budget, n_subcampaign, bids):
+        self.bids = bids
         self.n_bids = n_bids
         self.n_budget = n_budget
         self.n_subcampaign = n_subcampaign
         self.step1n = []
         self.step2n = self.step1n.append([[[] for j in range(self.n_bids)] for i in range(self.n_budget)])
+
         for i in range(n_subcampaign):
             self.step1n.append([[[] for j in range(self.n_bids)] for i in range(self.n_budget)])
             self.step1n[i] = np.random.randint(120, size=(self.n_bids, self.n_budget))
+
         self.finalm = [[[] for j in range(self.n_bids)] for i in range(self.n_budget)]
         self.result = 0
         self.first = True
 
-    def step1a(self,n):
-        if(self.first == True):
+    def step1a(self, n):
+        if (self.first == True):
             self.first = False
         else:
             self.step1n = n
-        print(self.step1n)
-        maxc = np.zeros(shape=(4,4))
+        #print("Initial matrix", self.step1n, "\n")
+        maxc = np.zeros(shape=(4, 4))
         for i in range(self.n_subcampaign):
-            maxc[i] = np.amax(self.step1n[i], axis = 1)
-        self.step2n = maxc
+            maxc[i] = np.amax(self.step1n[i], axis=1)
+        self.step2n = maxc #array with the maximum value of each row of the initial matrix
+        #print(self.step2n, "STEP2n")
         self.step2(self.step2n)
 
     def step2(self, step2bs):
 
-        self.finalm[0] = step2bs[0]
-        for i in range(1,self.n_budget):
-            for j in range(1, self.n_budget +1 ):
-                inv = self.finalm[i-1][0:j]
+        self.finalm[0] = step2bs[0]  #prima era self.finalm[0] = step2bs[0]
+        for i in range(1, self.n_budget):
+            for j in range(1, self.n_budget + 1):
+                inv = self.finalm[i - 1][0:j]
                 inv = inv[::-1]
                 res = np.add(inv, step2bs[i][0:j])
-                self.finalm[i][j-1] = np.amax(res)
-        print(self.finalm)
-        self.result = np.argmax(self.finalm, axis=1)
+                self.finalm[i][j - 1] = np.amax(res)
+        #print("Final matrix",self.finalm, "\n")
+        self.result = np.argmax(self.finalm, axis=1) #indici NELLA MATRICE DEI MASSSIMi dei valori massimi di quella riga
         return self.result
 
     def print_all(self):
-        print("SUB1 MATRIX ", self.step1n)
+        print("INIT MATRIX \n ", self.step1n)
         print("\n-------------\n")
-        print("VN MATRIX ", self.finalm)
+        print("Step2n MATRIX ,matrice dei massimi\n ", self.step2n)
         print("\n-------------\n")
-        print("RESULT ", self.result)
-        val = self.step2n[1][self.result[1]]
-        index = np.where(self.step1n[1][self.result[1]] == val)
-        print(index[0])
-        print("MAXVC", self.step2n)
+        print("FINAL MATRIX \n", self.finalm)
+        print("\n-------------\n")
+        print("Vettore con gli indici dei massimi di ogni riga della colonna finale\n ", self.result)
+        bids  = []
+        for i in range(self.n_subcampaign):
+            val = self.step2n[i][self.result[i]] #lo troviamo nella riga i-esima della matrice 2 e nella colonna data dall'indice dei massimi della matrice secondaria
+            print("Valore", val) # valore che ottimizza la subcampain
+            index = np.where(self.step1n[i][self.result[i]] == val)
+            bids.append(float(self.bids[index[0]]))
+           # print("INDICI", index[0]) #ora devo prendere il bid associato a quest'indice
+            #print ("Bid", self.bids[index[0]])
+            print("MAXVC", self.step2n)
+        print("BIDS OTTIMALI\n",bids)
---
+++ MatchingAdvertising/testknap.py
@@ -0,0 +1,7 @@
+from KnapOptimizer import *
+import numpy as np
+
+bids = np.linspace(start = 25, stop = 100, num = 4)
+knap = KnapOptimizer(n_bids=4, n_budget=4, n_subcampaign=4,bids = bids )
+knap.step1a([])
+knap.print_all()
